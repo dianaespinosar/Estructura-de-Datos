329172ed310551df03e359d1f7b05c1870cc43b8
dianaespinosar <40146288+dianaespinosar@users.noreply.github.com>
2019-02-25 17:05:29 -0600

    Update DePasajeros.java

--- problema10/DePasajeros.java
+++ problema10/DePasajeros.java
@@ -12,6 +12,11 @@ public class DePasajeros extends Camion{
         super(marca, numMotor, placas);
         this.totalPasaj = totalPasaj;
     }
+    
+    public DePasajeros(String marca, String numMotor, String placas, double costoCamion, int totalPasaj){
+       super(marca, numMotor, placas, costoCamion);
+       this.totalPasaj = totalPasaj;
+   }
 
    
     public String toString() {
@@ -28,6 +33,12 @@ public class DePasajeros extends Camion{
     public void setTotalPasaj(int totalPasaj) {
         this.totalPasaj = totalPasaj;
     }
+    
+    
+    public double calculaCostoServicio(double km){
+           double costoFinal = getCostoCamion() * .0001 / totalPasaj * km;
+           return costoFinal;
+       }
     
     
 }
