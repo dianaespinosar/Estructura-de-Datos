b284bf1ce117fabed4af99dd117feb7bf2e69e90
dianaespinosar <40146288+dianaespinosar@users.noreply.github.com>
2019-05-02 10:44:22 -0500

    Add files via upload

---
+++ ListaADT.java
@@ -0,0 +1,18 @@
+package Listas;
+
+import java.util.Iterator;
+
+public interface ListaADT<T> extends Iterable <T> {
+
+    public Iterator<T> iterador();
+    public boolean isEmpty();
+    public T quitaPrimero();
+    public T quitaUltimo();
+    public T quita(T dato);
+    public String toString();
+    public boolean contiene(T dato);
+    public int calcularTamaño();
+    public T obtienePrimero();
+    public T obtieneUltimo();
+    
+}
